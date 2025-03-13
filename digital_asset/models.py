15664edeed0147f4f4a7c37e1061dd01cafce73c
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-13 20:39:04 +0000

    Add CryptoAsset model to manage user crypto assets with relevant fields

--- digital_asset/models.py
+++ digital_asset/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class CryptoAsset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.DecimalField(max_digits=10, decimal_places=8)
+    purchase_date = models.DateField()
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
