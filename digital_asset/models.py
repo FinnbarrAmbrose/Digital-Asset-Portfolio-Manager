fc627e67036240915bd0dd0e9174930d4c155257
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-13 20:40:00 +0000

    Add methods for total value and profit/loss calculations in CryptoAsset model

--- digital_asset/models.py
+++ digital_asset/models.py
@@ -11,3 +11,12 @@ class CryptoAsset(models.Model):
     quantity = models.DecimalField(max_digits=10, decimal_places=8)
     purchase_date = models.DateField()
     current_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def total_value(self):
+        return self.quantity * self.current_price if self.current_price else None
+
+    def profit_loss(self):
+        return (self.current_price - self.purchase_price) * self.quantity if self.current_price else None
+
+    def __str__(self):
+        return f"{self.name} - {self.user.username}"
