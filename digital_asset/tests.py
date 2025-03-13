c3ebadd8805b26b594630f1131f80153ee19e8b8
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-03-13 20:51:40 +0000

    Add unit tests for CryptoAsset model creation

--- digital_asset/tests.py
+++ digital_asset/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import CryptoAsset
 
-# Create your tests here.
+class CryptoAssetTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="testuser")
+        CryptoAsset.objects.create(user=user, name="Bitcoin", purchase_price=50000, quantity=0.1)
+
+    def test_asset_creation(self):
+        asset = CryptoAsset.objects.get(name="Bitcoin")
+        self.assertEqual(asset.purchase_price, 50000)
